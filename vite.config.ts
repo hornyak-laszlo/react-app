b083f9b75822588f58082bec8b7d0713d445aa24
Laszlo Hornyak <laszlo.hornyak@superwert.com>
2025-07-17 22:13:57 +0200

    fix build issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import viteReact from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import { resolve } from 'node:path';
 
